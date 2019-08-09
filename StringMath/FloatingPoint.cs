90b29bf93bde756b914f3025748f9731adffdf88
Jim Doyle <indellibleink@gmail.com>
2019-08-08 20:33:43 -0400

    Moved Floating Point Logic to a new Class/Method to be coded

---
+++ StringMath/FloatingPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringMath
+{
+     public class FloatingPoint
+    {
+        public static void FindDecminslPoint()
+        { 
+        //Regex regex = new Regex(@"\W");
+        //Match matchNumer = regex.Match(express[0]);
+        //Match matchOper = regex.Match(express[2]);
+
+        //    if (matchNumer.Success == true)
+        //    {
+        //        double numer = double.Parse(express[0]);
+        //        double oper = double.Parse(express[2]);
+        //        Console.WriteLine("A:{0:R}    B: {1:R}", numer, oper);
+        //    }
+        //    else
+        //    {
+            //        int numer = int.Parse(express[0]);
+            //        int oper = int.Parse(express[2]);
+        //        Console.WriteLine("A:{0:R}    B: {1:R}", numer, oper);
+
+        }
+     }
+}
--- StringMath/Program.cs
+++ StringMath/Program.cs
@@ -7,47 +7,27 @@ namespace StringMath
     {
         static void Main(string[] args)
         {
+            int total;
            
             //Console.Write("Type out a Math equation: ");
             //string input = Console.ReadLine();
             
             string user = "11 + 11";
             Console.WriteLine("User: {0}", user);
+
             string[] express = user.Split(" ");
-
-            Regex regex = new Regex(@"\W");
-            Match matchNumer = regex.Match(express[0]);
-            Match matchOper = regex.Match(express[2]);
-
-            if (matchNumer.Success == true)
-            {
-                double numer = double.Parse(express[0]);
-                double oper = double.Parse(express[2]);
-                Console.WriteLine("A:{0:R}    B: {1:R}",numer, oper);
-            }
-            else
-            {
-                int numer = int.Parse(express[0]);
-                int oper = int.Parse(express[2]);
-                Console.WriteLine("A:{0:R}    B: {1:R}", numer, oper);
-
-            }
-
+            int numer = int.Parse(express[0]);
+            int oper = int.Parse(express[2]);
             char func = Convert.ToChar(express[1]);
-
-
-
-
 
             switch (func)
             {
-
                 case '+':
-                    Addition addThis = new Addition();
-                    addThis.AddItUp(numer, oper);
+                    total = numer + oper;
+                    Console.WriteLine(total);
                     break;
                 case '-':
-                 int   total = numer - oper;
+                    total = numer - oper;
                     Console.WriteLine(total);
                     break;
                 case '*':
